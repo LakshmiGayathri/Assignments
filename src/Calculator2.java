13d96202cd72f339bfe7b743f09fb08e97ac70ac
lakshmi <vasipalli.lucky@gmail.com>
2016-02-08 18:06:18 -0700

    Polymorphism

---
+++ src/Calculator2.java
@@ -0,0 +1,44 @@
+
+public class Calculator2 {
+
+  public int sum(int a,int b){
+	return a+b;	
+	}
+
+  public int sum(int a,int b,int c){
+	return a+b+c;
+   }
+  public int sum(int a,int b,int c,int d){
+	return a+b+c+d;
+   }
+  public int sum(int a,int b,int c,int d,int e){
+		return a+b+c+d+e;
+	   }
+  public int diff(int a,int b){
+	return a-b;
+   }
+  public int diff(int a,int b,int c){
+	return a-b-c;
+   }
+   public int diff(int a,int b,int c,int d){
+	return a-b-c-d;
+   }
+   public int diff(int a,int b,int c,int d,int e){
+		return a-b-c-d-e;
+	   }
+  public int product(int a,int b){
+	return a*b;
+   }
+
+	public int product(int a,int b,int c){
+		return a*b*c;
+	}
+   public int product(int a,int b,int c,int d){
+	return a*b*c*d;
+    }
+   public int product(int a,int b,int c,int d,int e){
+		return a*b*c*d*e;
+	    }
+}
+
+
