1b24f78a77da0d4a857814c14bfe016c2c4b7a5d
lakshmi <vasipalli.lucky@gmail.com>
2016-02-08 18:05:14 -0700

    Calculator

--- src/CalciUsage.java
+++ src/CalciUsage.java
@@ -3,8 +3,18 @@
 public class CalciUsage {
 	
 	public static void main(String[] args) {
-		Calculator  c = new ScientificCal();
-        c.sum();
+		Calculator2  c = new Calculator2();
+        System.out.println(c.sum(1,2));
+        System.out.println(c.product(4,2,5,7));
         
+        
+        ScientificCal s = new ScientificCal();
+        System.out.println(s.sum(1,2));
+        System.out.println(s.diff(3,2,7));
+        
+        
+        Calculator2  c1 = new ScientificCal();
+        System.out.println(c1.sum(1,2,3));
+        System.out.println(c1.diff(1,2,3));
 	}
 }
