13225c0c526a388264b7711183ee545579d36730
lakshmi <vasipalli.lucky@gmail.com>
2016-02-19 18:34:18 -0700

    Files Hierarchy

---
+++ src/Files/FilesList.java
@@ -0,0 +1,39 @@
+package Files;
+
+import java.io.*;
+import java.io.IOException;
+
+public class FilesList {
+    
+	public static void main(String[] args) throws IOException {
+		String s = "C:/Users/lakshmi/Desktop/learn/Files";
+		  File folder = new File(s);	  
+		  recursion(s,folder);
+	 }
+	  static int j=0;
+	  //Writes list of files n folders in a Specified folder
+	  public static void recursion(String s,File folder1){
+		      
+		      File[] listOfFiles = folder1.listFiles();
+		      for (int i = 0; i < listOfFiles.length; i++) {
+		        if (listOfFiles[i].isFile()) {	
+		          for(j=1;j>=1;j++){System.out.print("\t");}
+		          System.out.println("File:" + listOfFiles[i].getName());
+		        } else 
+		         if (listOfFiles[i].isDirectory()) {
+		          
+		          s = folder1.getAbsolutePath()+"/"+listOfFiles[i].getName();
+		          File folder2 = new File(s);
+		          j++;
+		          for(j=1;j>1;j++){System.out.print("\t");}
+		          System.out.println("Directory: " + listOfFiles[i].getName());	
+		          
+		          recursion(s,folder2);  //recursive method is called if it is folder
+		          
+		        }
+		      }
+		     j=0;
+		  } 
+	  	}
+
+
