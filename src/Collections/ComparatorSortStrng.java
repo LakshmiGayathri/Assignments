efe2cccfecbcceea0158119302e3b17779437b10
lakshmi <vasipalli.lucky@gmail.com>
2016-02-22 15:22:13 -0700

    Collections Sort String

---
+++ src/Collections/ComparatorSortStrng.java
@@ -0,0 +1,56 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.lang.*;
+
+public class ComparatorSortStrng {
+
+	public static void main(String[] args) {
+		ArrayList<String> aList=new ArrayList<String>();
+		aList.add("Hello");
+		aList.add(",");
+		aList.add("Welcome");
+		aList.add("to");
+		aList.add("Java");
+		aList.add("training");
+		
+		System.out.println("Before sorting");
+		for(String s:aList)
+		{	
+			System.out.print(s+" ");
+		}    
+		
+		Collections.sort(aList,new CharCount());
+		
+		System.out.println("\n\nSorted based on String length");
+		for(String s:aList)
+		{		
+			
+			System.out.print(s+" ");
+		}   
+		
+		    
+	}
+
+}
+
+class CharCount implements Comparator<String>{
+
+	//override Compare(arg 0, arg 1)
+	
+	public int compare(String a, String b) {
+		if(a.length()>b.length()){
+			return 1;
+		}else if(a.length()<b.length()){
+			return -1;
+		}
+		
+		return 0;
+	}
+}
